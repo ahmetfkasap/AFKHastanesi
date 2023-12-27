c2e1787beb3acafcc864eee1f4be31938403eb7e
Ahmet Kasap <kahmetf@gmail.com>
2023-12-27 23:52:10 +0300

    Layout ve admin controller düzenlendi.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AFKHastanesi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AFKHastanesi.Controllers
@@ -7,6 +8,42 @@ namespace AFKHastanesi.Controllers
     public class AdminController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DoktorEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DoktorEkle(Doktor doktor)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PoliklinikEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PoliklinikEkle(Poliklinik poliklinik)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DoktorListele()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PoliklinikListele()
         {
             return View();
         }
